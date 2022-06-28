6c9a3ef376a10a5d7ca894beecbe35abb55a870a
FrugalCarrot7 <james.devine1995@gmail.com>
2022-06-28 10:50:57 -0700

    adds create a team

--- controllers/teams.js
+++ controllers/teams.js
@@ -2,6 +2,8 @@ var Team = require('../models/team');
 
 module.exports = {
     index,
+    new: newTeam,
+    create,
 };
 
 function index(req, res){ 
@@ -10,4 +12,19 @@ function index(req, res){
       res.render('teams/index', { title: 'All Teams', teams });
     });
 
+}
+
+function newTeam(req, res){
+  res.render('teams/new', { title: 'ADD TEAM' });
+}
+
+function create(req, res) {
+  var team = new Team(req.body);
+  team.save(function(err) {
+    // one way to handle errors
+    if (err) return res.redirect('/teams/new');
+    console.log(team);
+    // for now, redirect right back to new.ejs
+    res.redirect(`/teams`);
+  });
 }
--- routes/teams.js
+++ routes/teams.js
@@ -3,5 +3,7 @@ var router = express.Router();
 var teamsCtrl = require('../controllers/teams');
 
 router.get('/', teamsCtrl.index);
+router.get('/new', teamsCtrl.new);
+router.post('/', teamsCtrl.create);
 
 module.exports = router;
