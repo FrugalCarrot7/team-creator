7ad593b8ee65561a4681adf312394423c5abb283
FrugalCarrot7 <james.devine1995@gmail.com>
2022-07-01 08:41:03 -0700

    protects routes

--- routes/teams.js
+++ routes/teams.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var teamsCtrl = require('../controllers/teams');
+const isLoggedIn = require('../config/auth');
 
 router.get('/', teamsCtrl.index);
 router.get('/new', teamsCtrl.new);
 router.get('/:id', teamsCtrl.show);
-router.post('/', teamsCtrl.create);
-router.delete('/:id', teamsCtrl.delete);
-router.get('/:id/edit', teamsCtrl.edit);
-router.put('/:id', teamsCtrl.update);
+router.post('/', isLoggedIn, teamsCtrl.create);
+router.delete('/:id', isLoggedIn, teamsCtrl.delete);
+router.get('/:id/edit', isLoggedIn, teamsCtrl.edit);
+router.put('/:id', isLoggedIn, teamsCtrl.update);
 
 module.exports = router;
